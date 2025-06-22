ffa31d423efcaf8d804da1caa5d55fc65d79873d
David Y. Stephenson <david@davidystephenson.com>
2025-06-22 12:58:48 -0400

    Complete products router

--- app/routes/productRoutes.js
+++ app/routes/productRoutes.js
@@ -1,1 +1,16 @@
-/* WRITE YOUR CODE HERE */
+const express = require('express')
+const middleware = require('../middleware/authenticationHandler')
+const service = require('../services/productService')
+
+const router = express.Router()
+
+const root = router.route('/')
+root.get(middleware.auth, service.getAllProducts)
+root.post(middleware.auth, service.createProduct)
+
+const id = router.route('/:id')
+root.get(middleware.auth, service.getProduct)
+root.put(middleware.auth, service.editProduct)
+root.delete(middleware.auth, service.deleteProduct)
+
+module.exports = router
